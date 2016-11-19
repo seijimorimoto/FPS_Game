669b0ec7d079c707b0c2fc3986d8a8305b2b4df4
Seiji <seijimorimoto@users.noreply.github.com>
2016-11-18 23:46:28 -0600

    Commit for setting up empty project
    Empty project with just one incompleted version of the "Change Weapon"
    script

---
+++ Assets/Scripts/changeWeapon.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class changeWeapon : MonoBehaviour {
+
+	public int iCurrentWeapon;
+	public GameObject[] goWeapons;
+
+	private int iTotalWeapons;
+	private HashSet<int> hSetOfWeapons;
+
+	private void deactivateAllWeapons(){
+		for (int iX = 0; iX < goWeapons.Length; iX++)
+			goWeapons [iX].SetActive (false);
+	}
+
+	private void activateWeapon(int iNum){
+		if (hSetOfWeapons.Contains (iNum))
+		{
+			goWeapons [iCurrentWeapon].SetActive (false);
+			goWeapons [iNum].SetActive (true);
+			iCurrentWeapon = iNum;
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		deactivateAllWeapons ();
+		iCurrentWeapon = 0;
+		iTotalWeapons = 1;
+		hSetOfWeapons = new HashSet<int> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (Input.GetKey (KeyCode.Alpha1))
+			activateWeapon (1);
+		else if (Input.GetKey (KeyCode.Alpha2))
+			activateWeapon (2);
+		// Hacer los else ifs necesarios para todas las armas que se puedan tener en el juego.
+	}
+}
